e5d3b7537c5da7bb1004ff1a229ccfdb7a1f9698
Sergio Oseko <osk.sergio@gmail.com>
2022-12-27 06:30:11 -0300

    feat: create scaffold conference

---
+++ app/controllers/conferences_controller.rb
@@ -0,0 +1,51 @@
+class ConferencesController < ApplicationController
+  before_action :set_conference, only: [:show, :update, :destroy]
+
+  # GET /conferences
+  def index
+    @conferences = Conference.all
+
+    render json: @conferences
+  end
+
+  # GET /conferences/1
+  def show
+    render json: @conference
+  end
+
+  # POST /conferences
+  def create
+    @conference = Conference.new(conference_params)
+
+    if @conference.save
+      render json: @conference, status: :created, location: @conference
+    else
+      render json: @conference.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /conferences/1
+  def update
+    if @conference.update(conference_params)
+      render json: @conference
+    else
+      render json: @conference.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /conferences/1
+  def destroy
+    @conference.destroy
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_conference
+      @conference = Conference.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def conference_params
+      params.require(:conference).permit(:description)
+    end
+end
---
+++ db/migrate/20221227092711_create_conferences.rb
@@ -0,0 +1,9 @@
+class CreateConferences < ActiveRecord::Migration[6.1]
+  def change
+    create_table :conferences do |t|
+      t.string :description
+
+      t.timestamps
+    end
+  end
+end
