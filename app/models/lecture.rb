47e627adf8661a9312e0557dc801dca0f5ea81a4
Sergio Oseko <osk.sergio@gmail.com>
2023-01-04 16:47:12 -0300

    feat: validations implemented in the Lecture class

--- app/models/lecture.rb
+++ app/models/lecture.rb
@@ -1,4 +1,6 @@
 class Lecture < ApplicationRecord
   belongs_to :conference
   belongs_to :track
+  validates :description, presence: true
+  validates :description, length: { minimum: 3, maximum: 200 }
 end
