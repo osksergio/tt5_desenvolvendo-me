603107d2cef14834e048dd04f758f2b73aeb0046
Sergio Oseko <osk.sergio@gmail.com>
2023-01-05 14:51:23 -0300

    feat: define the associations in the models

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,5 +1,6 @@
 class Track < ApplicationRecord
   belongs_to :conference
+  has_many :lectures
   validates :description, presence: true
   validates :description, length: { minimum: 3, maximum: 100 }
 end
