e27b3ce256a3410dcf56f6016033a9cd41f82f55
Sergio Oseko <osk.sergio@gmail.com>
2023-01-10 22:57:26 -0300

    feat: starting implementation of services

---
+++ app/services/minutes_lecture.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+class MinutesLecture
+  def initialize(str)
+    @str = str
+  end
+end
---
+++ app/services/total_minutes.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+class TotalMinutes
+  def initialize(arr)
+    @arr = arr
+  end
+
+  def totalize
+    total = 0
+    minutes_lecture = 'CRIAR UM SERVICE PARA RETORNAR OS MINUTES DA PALESTRA'
+
+    # looping no array totalizando os minutos
+  end
+end
+
